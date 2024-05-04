de58f4ca048583f01aa9afddb59bd525511e4d8f
Aparicio Junior <apariciojunior11@gmail.com>
2024-05-04 16:31:52 -0300

    habilitando hot reload para trabalhar com docker

--- backend2/app/config/db.py
+++ backend2/app/config/db.py
@@ -2,10 +2,10 @@ import os
 from sqlalchemy import create_engine, MetaData
 from sqlalchemy.orm import sessionmaker, Session
 
-DB_USER =  "root" #os.getenv("DB_USER") #"root"
-DB_PASSWORD = "RootPassword" #os.getenv("DB_PASSWORD") #"RootPassword"
-DB_HOST = "localhost" # os.getenv("DB_HOST") "kanastra-database"
-DB_NAME = "Kanastra" #os.getenv("DB_NAME")
+DB_USER =  os.getenv("DB_USER") 
+DB_PASSWORD = os.getenv("DB_PASSWORD") 
+DB_HOST = os.getenv("DB_HOST")
+DB_NAME = os.getenv("DB_NAME")
 
 DATABASE_URL = f"mysql+mysqlconnector://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
 
