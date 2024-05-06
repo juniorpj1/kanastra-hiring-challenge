8adce32079927c85dac5420d0afb083fb61603bc
Aparicio Junior <apariciojunior11@gmail.com>
2024-05-06 15:06:05 -0300

    implementando testes unitários com unittest, pytest, starlette

--- backend2/app/routes/routes.py
+++ backend2/app/routes/routes.py
@@ -1,20 +1,26 @@
-from fastapi import APIRouter, Depends, status
+from fastapi import APIRouter, Depends, HTTPException, status
 from fastapi.responses import JSONResponse
 from starlette.responses import RedirectResponse
 from app.schemas.schemas import Charges
 from app.services.service import ChargeService
 from sqlalchemy.orm import Session
 from app.config.db import get_db
+from fastapi import Request
 
 charge = APIRouter()
 
 @charge.get("/")
-def read_root():
-    response = RedirectResponse(url='http://localhost:8080')
-    return response
+def read_root(request: Request):
+    # Verifica se a solicitação já veio de 'http://localhost:8080'
+    if "localhost:8080" in str(request.url):
+        return JSONResponse(content={"message": "Você já está na página inicial!"})
+    else:
+        response = RedirectResponse(url='http://localhost:8080')
+        return response
 
 @charge.post("/charges", status_code=status.HTTP_201_CREATED)
 async def create_charge(charge: Charges, db: Session = Depends(get_db)):
-    new_charge = ChargeService.create(db, charge)
-    return JSONResponse(status_code=status.HTTP_201_CREATED, 
-                        content={"message": "Charge created successfully", "charge": new_charge})
+        new_charge = ChargeService.create(db, charge)
+        return JSONResponse(status_code=status.HTTP_201_CREATED, 
+                            content={"message": "Charge created successfully", "charge": new_charge})
+    
